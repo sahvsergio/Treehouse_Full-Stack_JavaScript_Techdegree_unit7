d0977b0f4886430be221d7f4c56283f7938128d2
sahvsergio <sergioandresherrera@gmail.com>
2025-03-20 20:23:43 -0500

    correct issues with rendering

--- react-gallery/src/components/NotFound.js
+++
@@ -1,8 +0,0 @@
-const NotFound = () => {
-  return (
-    <li class="not-found">
-      <h3>No Results Found</h3>
-      <p>You search did not return any results. Please try again.</p>
-    </li>
-  );
-};
