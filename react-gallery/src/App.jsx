5970ac3e722194f368a75197d55b4182616bb10b
sahvsergio <sergioandresherrera@gmail.com>
2025-03-24 22:17:54 -0500

    create home route and routes for cats, dogs, computers

--- react-gallery/src/App.jsx
+++ react-gallery/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
+
+
+import { BrowserRouter} from 'react-router-dom'
+
+
 import viteLogo from '/vite.svg'
 import './App.css'
 import apiKey from './config'
+
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
 import PhotoList from './components/Photolist';
 import Photo from './components/Photo';
+
+
+
+
 
 const myKey=apiKey;
 
@@ -19,19 +30,39 @@ function fetchData(query) {
 
 
 };
+{/*
+    / - Home route: when visiting the home route, the user should be redirected to the first static route.
+    3 static routes for default topics, for example /cats, /dogs and /computers. These will be used in the navigation component, feel free to customize these topics. These should render the PhotoList component.
+    /search/:query - route to handle user search queries. This should render the PhotoList component.
+    Use <NavLink> components in your Nav component for the 3 default topics.
+    The current route should always be reflected in the URL.
+*/}
 
 
 const App=()=> {
-  
 
- 
+
+
 
 
   return (
- 
-<>      {/* Add a */}
-    <SearchForm />
-    <Nav />
+<>
+<Nav/>     
+
+<Routes>
+
+        <Route path='/*' element={<SearchForm />}/>
+        <Route index element={<Navigate replace to='/cats'/>}/>
+        <Route path='dogs'/>   
+        <Route path='/computers'/>   
+</Routes>
+
+
+
+
+
+    
+  
     <PhotoList />
     </>
 
--- react-gallery/src/components/Nav.jsx
+++ react-gallery/src/components/Nav.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { useState, useEffect, } from 'react'
+
+import { useState, useEffect, } from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+
+
+
+import fetchData from  '../App'
+
+
 const Nav = () => {
     return (
             
         <nav className="main-nav">
             <ul>
-                <li><a href='#'>Cats</a></li>
-                <li><a href='#'>Dogs</a></li>
-                <li><a href='#'>Computers</a></li>
+                <li><NavLink to='/cats'>Cats</NavLink></li>
+                <li><NavLink to='/dogs'>Dogs</NavLink></li>
+                <li><NavLink to='/computers'>Computers</NavLink></li>
             </ul>
         </nav>
+
     );
 }
 export default Nav;
